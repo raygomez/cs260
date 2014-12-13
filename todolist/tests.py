de2350e4b17a26a9245dbc644fe1a86cda0db5db
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-13 21:10:40 +0800

    fix create instance test

--- todolist/tests.py
+++ todolist/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from todolist.models import ToDo
 
 class TodoList(TestCase):
 
     def test_create_instance_of_to_do(self):
-        todo = Todo()
+        todo = ToDo()
