2ca68384f6fc63bf26de2d3a748c404e0a53e21c
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-13 23:05:15 +0800

    added test to display date

---
+++ todolist/test_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+import datetime
+
+class TodoListView(TestCase):
+
+    def test_shows_current_date_in_index(self):
+        resp = self.client.get(reverse('todolist:index'))
+        dateToday = datetime.date.today()    
+        self.assertContains(resp, dateToday.strftime('%m-%d-%Y'))
