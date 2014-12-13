e022b8f95c814545599ec7f4b8c6d9b87712e072
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-14 01:50:35 +0800

    fix date format

--- todolist/test_views.py
+++ todolist/test_views.py
@@ -7,5 +7,5 @@ class TodoListView(TestCase):
     def test_shows_current_date_in_index(self):
         resp = self.client.get(reverse('todolist:index'))
         dateToday = datetime.date.today()    
-        self.assertContains(resp, dateToday.strftime('%m-%d-%Y'))
+        self.assertContains(resp, dateToday)
         self.assertTemplateUsed(resp, 'index.html')
