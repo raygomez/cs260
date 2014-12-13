d7357e513ea0d30705267d56216ba87c1c1d10f2
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-13 21:37:21 +0800

    remove redundant test

--- todolist/tests.py
+++ todolist/tests.py
@@ -6,6 +6,3 @@ class TodoList(TestCase):
     def test_create_instance_of_to_do(self):
         todo = ToDo()
 
-    def test_if_app_is_accessible(self):
-        resp = self.client.get('/todolist/');
-        self.assertEqual(200, resp.status_code)
