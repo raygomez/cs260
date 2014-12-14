847d86566f0bf33e4772e1be79dca15d06ff578e
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-15 00:20:24 +0800

    added test for showing more than one todo list

--- todolist/test_views.py
+++ todolist/test_views.py
@@ -24,3 +24,22 @@ class TodoListView(TestCase):
         self.assertContains(resp, 'False')
         self.assertTemplateUsed(resp, 'index.html')
         
+    def test_show_two_todo_index(self):
+        ToDo.objects.create(description='todo list 1', isDone=False,
+                                   dateAdded=datetime.date.today())
+        ToDo.objects.create(description='todo list 2', isDone=True,
+                                   dateAdded=datetime.date.today())
+
+        dateToday = datetime.date.today()    
+
+        resp = self.client.get(reverse('todolist:index'))
+
+        self.assertTemplateUsed(resp, 'index.html')
+        self.assertContains(resp, dateToday.strftime('%b %d, %Y'))
+
+        self.assertContains(resp, 'todo list 1')
+        self.assertContains(resp, 'False')
+        self.assertTemplateUsed(resp, 'index.html')
+
+        self.assertContains(resp, 'todo list 2')
+        self.assertContains(resp, 'True')
