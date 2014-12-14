4c71763cd4512be097cf09122f9a08b94f14b752
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-15 00:20:54 +0800

    rename test

--- todolist/test_views.py
+++ todolist/test_views.py
@@ -6,7 +6,7 @@ import datetime
 
 class TodoListView(TestCase):
 
-    def test_shows_current_date_in_index(self):
+    def test_shows_no_todo_index(self):
         resp = self.client.get(reverse('todolist:index'))
         dateToday = datetime.date.today()    
         self.assertContains(resp, dateToday.strftime('%b %d, %Y'))
