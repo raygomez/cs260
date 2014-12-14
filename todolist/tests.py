c497d280d3bc81c226b41e77b9d4eeef6ca91464
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-14 17:09:37 +0800

    updated create instance test

--- todolist/tests.py
+++ todolist/tests.py
@@ -4,5 +4,8 @@ from todolist.models import ToDo
 class TodoList(TestCase):
 
     def test_create_instance_of_to_do(self):
-        todo = ToDo()
-
+        todo = ToDo.objects.create(description='todo list 1', isDone=False,
+            dateAdded=datetime.date.today())
+        self.assertEqual("todo list 1", todo.description)
+        self.assertTrue(not(todo.isDone))
+        self.assertEqual(datetime.datetoday(), todo.dateAdded)
