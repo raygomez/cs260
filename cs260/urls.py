30708e3265fb9225897c8e00ceed5a9cb40efe11
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-18 04:30:26 +0800

    redirect to login after logout

--- cs260/urls.py
+++ cs260/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^todolist/', include('todolist.urls', namespace="todolist")),
     url(r'^$', include('todolist.urls', namespace="todolist")),
