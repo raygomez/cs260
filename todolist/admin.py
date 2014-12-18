d7d0fa347e23a1931a94115674084fc4a8131a90
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-18 11:41:52 +0800

    remove default in user

--- todolist/admin.py
+++ todolist/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from todolist.models import ToDo
 
 class ToDoAdmin(admin.ModelAdmin):
-    fields = ['description', 'dateAdded', 'isDone']
-    list_display = ('description', 'dateAdded', 'isDone')
+    fields = ['description', 'dateAdded', 'isDone', 'user']
+    list_display = ('description', 'dateAdded', 'isDone', 'user')
     
 admin.site.register(ToDo, ToDoAdmin)
--- todolist/models.py
+++ todolist/models.py
@@ -7,4 +7,4 @@ class ToDo(models.Model):
     description = models.CharField(max_length=200, default="", blank=False)
     isDone = models.BooleanField(default=False)
     dateAdded = models.DateField(default=datetime.date.today())
-    user = models.ForeignKey(User, blank=False, default=1)
+    user = models.ForeignKey(User, blank=False)
