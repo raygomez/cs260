d136562fff070d7a3a8c6ddfef1298182bda6153
Ray Gomez <rayvincent.gomez@gmail.com>
2014-12-18 15:32:21 +0800

    remove description

--- todolist/forms/AddToDoForm.py
+++ todolist/forms/AddToDoForm.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
 from todolist.models import ToDo
+from django.utils.translation import ugettext_lazy as _
 
 class AddToDoForm(ModelForm):
     class Meta:
         model = ToDo
         exclude = ['isDone', 'dateAdded', 'user']
+        labels = {
+            'description' : _(' '),
+        }
